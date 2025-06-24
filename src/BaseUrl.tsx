606d56bb5f756aec3d52c257f7851bce59f8f425
sharvani67 <sharvanikokkonda@gmail.com>
2025-06-24 17:19:57 +0530

    added some changes

--- src/BaseUrl.tsx
+++ src/BaseUrl.tsx
@@ -1,3 +1,7 @@
-const baseURL: string = 'http://localhost:5000/api';
+// const baseURL: string = 'http://localhost:5000/api';
+const baseURL: string = 'https://mechanicsince1999.com:4000/api';
+
+
+
 
 export default baseURL;
